603eeef3bcdf9b85a6cb2384907b103780859d41
unknown <geerman.f@gmail.com>
2018-05-15 22:33:03 -0300

    estructura inicial del proyecto

---
+++ src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { IniciarSesionComponent } from './iniciarSesion/iniciarSesion.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: IniciarSesionComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
---
+++ src/app/iniciarSesion/iniciarSesion.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './iniciarSesion.component.html'
+})
+export class IniciarSesionComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {}
+}
