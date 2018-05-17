96cdd4942a9b382631713ddcb4d83d23b0e63ca1
antonella <antonella.sara.r@gmail.com>
2018-05-17 16:41:15 -0300

    formulario registrarse

--- src/app/registrarse/registrarse.component.ts
+++ src/app/registrarse/registrarse.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-registrarse',
-    templateUrl: './registrarse.component.html'
+    templateUrl: './registrarse.component.html',
+    styleUrls: ['./registrarse.component.css']
 })
 export class RegistrarseComponent implements OnInit {
     constructor() { }
