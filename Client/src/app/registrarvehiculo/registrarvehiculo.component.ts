da8d66d2526a58be20a4c2e2c3d39728c024e5f0
antonella <antonella.sara.r@gmail.com>
2018-05-24 02:25:27 -0300

    cambios

---
+++ Client/src/app/registrarvehiculo/registrarvehiculo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrarvehiculo',
+  templateUrl: './registrarvehiculo.component.html',
+  styleUrls: ['./registrarvehiculo.component.css']
+})
+export class RegistrarvehiculoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
